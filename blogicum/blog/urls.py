6aedcbf0953dbd410512983bdf3a48cf1096038e
Минаев Александр Викторович <fonvisinoff@yandex.ru>
2024-12-08 15:22:34 +0300

    First commit

---
+++ blogicum/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('posts/<int:pk>/', views.post_detail),
+    path('category/<slug:category>/', views.category_posts),
+] 
